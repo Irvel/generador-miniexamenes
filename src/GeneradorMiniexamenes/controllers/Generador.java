280e0926c5e0c5b75762ecf6e83b86fa1f24dfdc
Irvel <irvel.nduva@gmail.com>
2016-11-06 16:07:23 -0600

    Initial commit

---
+++ src/GeneradorMiniexamenes/controllers/Generador.java
@@ -0,0 +1,7 @@
+package GeneradorMiniexamenes.controllers;
+
+/**
+ * Created by Irvel on 11/6/16.
+ */
+public class Generador {
+}
---
+++ src/GeneradorMiniexamenes/model/BancoDePreguntas.java
@@ -0,0 +1,7 @@
+package GeneradorMiniexamenes.model;
+
+/**
+ * Created by Irvel on 11/6/16.
+ */
+public class BancoDePreguntas {
+}

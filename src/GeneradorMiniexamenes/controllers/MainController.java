f6cd9bd4a7e2a3571f27817b5bdbf8f259dfe189
ElaisM <a01280762@itesm.mx>
2016-11-13 17:03:57 -0600

    Add TabGenera features
    Checks if there is at least one Subject, if not disables the Generate
    button.
    Adds available subjects on ComboBox
    Adds quantities to Cantidad ComboBox

--- src/GeneradorMiniexamenes/controllers/MainController.java
+++ src/GeneradorMiniexamenes/controllers/MainController.java
@@ -2,7 +2,12 @@ package GeneradorMiniexamenes.controllers;
 
 import GeneradorMiniexamenes.model.ExamBank;
 import GeneradorMiniexamenes.model.QuestionBank;
+import GeneradorMiniexamenes.model.Subject;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 
 /**
  * Created by Elias Mera on 11/7/2016.
@@ -14,6 +19,7 @@ public class MainController {
     private Generate mGenerate;
     private QuestionBank mQuestionBank;
     private ExamBank mExamBank;
+    private boolean bInicio = true;
 
     /**
      * Initializer of MainController
@@ -51,5 +57,39 @@ public class MainController {
         //mExport.onClick(actionEvent, mQuestionBank);
         // Save the generated exams to storage
         AppState.saveExamBank(mExamBank);
+    }
+
+    @FXML JFXComboBox cbTema;
+    @FXML JFXComboBox cbCantidad;
+    @FXML JFXButton btnGenerate;
+
+    /**
+     * tabGenera
+     *
+     * Method that loads subjects every time the user clicks on tabGeneraExamen
+     * Disables Genera button if there are no subjects
+     *
+     * @param event
+     */
+    public void tabGenera(Event event) {
+        // Checks if there is at least one topic
+        if(mQuestionBank.getSubjects().isEmpty()){
+            btnGenerate.setDisable(true);
+        }
+        else{
+            btnGenerate.setDisable(false);
+            // Loads subjects
+            for(Subject s : mQuestionBank.getSubjects()) {
+                if (!cbTema.getItems().contains(s.getSubjectName()))
+                    cbTema.getItems().add(s.getSubjectName());
+            }
+
+            // loads quantities only on start
+            if(bInicio) {
+                for (int i = 1; i <= 35; i++)
+                    cbCantidad.getItems().add(i);
+                bInicio = !bInicio;
+            }
+        }
     }
 }
